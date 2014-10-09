659fd88da7951d3359d5ecfcb3ae61a5369601cb
grantholly <gdholly@gmail.com>
2014-10-09 00:26:24 -0700

    adding a little bit more to closures examples

--- code-guild/javascript/closures.js
+++ code-guild/javascript/closures.js
@@ -20,3 +20,47 @@ var titleGreeting = function(greeting) {
 var greet = titleGreeting("salutations");
 greet("Grant");
 
+/*IIFE
+immediately invoked function expressions
+allows the state of iteration to be frozen in the inner scope of the returned function
+inside the parent function*/
+
+var createFunction = function(i) {
+
+    return function() {
+	alert(i);
+    };
+};
+
+/*i would otherwise only be accessible with its last value after looping
+so to "save" the state of i in between*/ 
+
+for (var i = 0; i < 5; i++) {
+   setTimeout(createFunction(i), i * 100);
+}
+
+// Using a closure to access inner and outer object instances simultaneously.
+
+var outerObj = {
+    myName: "outer",
+    outerFunction: function() {
+
+// Provide a reference to outerObj through innerFunction's closure
+
+    var self = this;
+    var innerObj = {
+	myName: "inner",
+	innerFunction: function() {
+	    console.log( self.myName, this.myName ); // "outer inner"
+        }
+    };
+
+    innerObj.innerFunction();
+    
+    console.log( this.myName ); // "outer"
+    }
+};
+
+outerObj.outerFunction();
+
+
