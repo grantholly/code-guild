9f2c88106ce99770b776b5b13f71fdfc52893de7
grantholly <gdholly@gmail.com>
2014-10-31 12:10:32 -0700

    adding demo django app

---
+++ code-guild/demo/demo/views.py
@@ -0,0 +1,57 @@
+from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
+from django.contrib import auth
+from django.core.context_processors import csrf
+
+from .forms import MyRegistrationForm
+
+def login(request):
+    
+    token = {}
+    token.update(csrf(request))
+    return render_to_response('login.html', token)
+
+def authenticate(request):
+    
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+    user = auth.authenticate(username=username, password=password)
+
+    if user is not None:
+	auth.login(request, user)
+	return HttpResponseRedirect('/login_success')
+    else:
+	return HttpResponseRedirect('/invalid')
+
+def login_success(request):
+
+    return render_to_response('login_success.html', {'first_name': request.user.username})
+
+def invalid(request):
+
+    return render_to_response('invalid.html')
+
+def logout(request):
+
+    auth.logout(request)
+    return render_to_response('logout.html')
+
+def register(request):
+
+    if request.method == 'POST':
+	form = MyRegistrationForm(request.POST)
+	if form.is_valid():
+	    form.save()
+	    return HttpResponseRedirect('/register_success')
+    else:
+	form = MyRegistrationForm()
+    token = {}
+    token.update(csrf(request))
+    token['form'] = form
+
+    return render_to_response('register.html', token)
+
+def register_success(request):
+
+    return render_to_response('register_success.html')
+
---
+++ code-guild/demo/signups/migrations/0001_initial.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='SignUp',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('first_name', models.CharField(max_length=100, null=True, blank=True)),
+                ('last_name', models.CharField(max_length=100, null=True, blank=True)),
+                ('email', models.EmailField(max_length=75)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
