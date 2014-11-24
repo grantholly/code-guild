69984e250d7b2af3d07a5cf5a50b459a0de6aabd
grantholly <gdholly@gmail.com>
2014-11-23 20:53:11 -0800

    finished comment display but still need CSS for comments

--- code-guild/demo/static/blog/js/comments.js
+++ code-guild/demo/static/blog/js/comments.js
@@ -30,15 +30,25 @@ $(document).ready(function () {
     });
 
     //AJAX GET request comment handler
-    function getComments (blogId) {
-	$.ajax({
-	    "method": "GET",
-	    "url": "get_comments/",
-	    "data": {"blogId": blogId},
-	    "success": function (data) {
-		//update the DOM with related comments
-	    }
-	})
+    function getComments (blogId, $comments) {
+	//if there aren't any currently loaded comments in the dom, make AJAX GET request
+	if ($comments.children().length === 0) {
+	    $.ajax({
+                "method": "GET",
+                "url": "get_comments/",
+                "data": {"blogId": blogId},
+                "success": function (data) {	    
+		    var i,
+			end = data.length,
+			$commentWrapper = $("<div></div>")
+		    for (i = 0; i < end; i++) {
+			$comments.append($("<p></p>").html(data[i].fields.created).addClass("comment-created"));
+			$comments.append($("<div></div>").html(data[i].fields.body).addClass("comment-body"));
+		    }
+                }
+            })
+	}
+	$comments.toggle("fast");
     };
 
     //AJAX POST request comment handler
@@ -80,8 +90,9 @@ $(document).ready(function () {
 
     //get comments
     $(".get-comments").click(function () {
-        var blogId = this.id.split("-")[2];
+        var blogId = this.id.split("-")[2],
+	    $comments = $("#blog-" + blogId + "-comments");
 	//send AJAX GET request for comments
-        getComments(blogId);
+       	getComments(blogId, $comments);
     });
 });
