be065790e006a8f82f6e5260852bb21bff980c77
grantholly <gdholly@gmail.com>
2014-11-29 16:04:09 -0800

    fixed blog deatil urls

--- code-guild/demo/blog/urls.py
+++ code-guild/demo/blog/urls.py
@@ -4,9 +4,8 @@ from blog.views import BlogIndex
 from . import views
 
 urlpatterns = patterns('',
-    #blog urls
     url(r'^$', BlogIndex.as_view(), name='index'),
-    url(r'(?P<pk>\d+)/$', views.blog_post_detail, name='detail'),
+    url(r'(?P<pk>\d+)/(?P<slug>[\w-]+)/$', views.blog_post_detail, name='detail'),
     url(r'vote/$', views.vote, name='vote'),
     url(r'add_comment/$', views.add_comment, name='add_comment'),
     url(r'get_comments/$', views.get_comments, name='get_comments'),
